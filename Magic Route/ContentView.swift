48eac65a8e0cf067e354d8caca7a8d9ccfc83843
Flacko <feliciafarq@gmail.com>
2026-03-18 22:32:48 -0700

    Initial Commit

---
+++ Magic Route/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magic Route
+//
+//  Created by Flacko Farquharson on 3/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magic Route/Magic_RouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_RouteApp.swift
+//  Magic Route
+//
+//  Created by Flacko Farquharson on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Magic_RouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
